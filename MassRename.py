68048a801c67d768bfff7958b19a29af4a008050
etienne <etienne@pop-os.localdomain>
2020-12-06 18:04:24 +0200

    started making the app into something usefil

--- MassRename.py
+++ MassRename.py
@@ -1,32 +1,49 @@
 import os
 import sys
 
-walk_dir = sys.argv[1]
+def help():
+	print("HELP")
 
-print('walk_dir = ' + walk_dir)
+def goRename(walk_dir,renameExt,renameExtFilter,contains):
+	print('walk_dir = ' + walk_dir)
+	print('renameExt = ' + renameExt)
+	print('renameExtFilter = ' + renameExtFilter)
+	print('walk_dir (absolute) = ' + os.path.abspath(walk_dir))
 
-# If your current working directory may change during script execution, it's recommended to
-# immediately convert program arguments to an absolute path. Then the variable root below will
-# be an absolute path as well. Example:
-# walk_dir = os.path.abspath(walk_dir)
-print('walk_dir (absolute) = ' + os.path.abspath(walk_dir))
+	for root, subdirs, files in os.walk(walk_dir):
 
-for root, subdirs, files in os.walk(walk_dir):
+		for filename in files:
+			file_path = os.path.join(root, filename)
+			base=os.path.basename(file_path)
+			thefile=os.path.splitext(base)[0]
+			if renameExtFilter != '':			
+				if contains:
+					if os.path.splitext(base)[1].count(renameExtFilter)>-1:
+						os.rename(file_path, os.path.join(root, thefile+'.'+renameExt))
+				else:
 
-	for filename in files:
-		file_path = os.path.join(root, filename)
-		base=os.path.basename(file_path)
-		thefile=os.path.splitext(base)[0]
-		if os.path.splitext(base)[1]=='.002':
-			os.remove(file_path)
-			continue
-		if os.path.splitext(base)[1]=='.001':
-			os.rename(file_path, os.path.join(root, thefile+'.jar'))
-		#dst ="Hostel" + str(count) + ".jpg"
-       # src ='xyz'+ filename 
-       # dst ='xyz'+ dst 
-          
-        # rename() function will 
-        # rename all the files 
-         
-		
+					if os.path.splitext(base)[1]=='.'+renameExtFilter:
+						os.rename(file_path, os.path.join(root, thefile+'.'+renameExt))
+			else:
+					os.rename(file_path, os.path.join(root, thefile+'.'+renameExt))
+
+
+if __name__ == "__main__":
+
+	if len(sys.argv) <2:
+		help()
+		exit()
+
+
+	walk_dir = sys.argv[1]
+	renameExt= sys.argv[2]
+	renameExtFilter= ''
+	contains=0
+
+	if len(sys.argv) >3:
+		renameExtFilter=sys.argv[3]
+
+	if len(sys.argv) >4:
+		contains=sys.argv[4]
+
+	goRename(walk_dir,renameExt,renameExtFilter,contains)
