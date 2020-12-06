b7d5f4f2809de69682b91c808f22d300d08a403f
etienne <etienne@pop-os.localdomain>
2020-12-06 21:50:09 +0200

    good enought to begin using it

--- MassRename.py
+++ MassRename.py
@@ -2,40 +2,47 @@ import os
 import sys
 
 def help():
-	print("HELP")
+	print("Usage: python3 MassRename.py m1 m2 o1 o2 o3")
+	print("Mandatory parameters:")
 
-def goRename(walk_dir,renameExt,renameExtFilter,contains):
-	print('walk_dir = ' + walk_dir)
-	print('renameExt = ' + renameExt)
-	print('renameExtFilter = ' + renameExtFilter)
-	print('walk_dir (absolute) = ' + os.path.abspath(walk_dir))
+	print("m1: The base directory: [String] The base directory the script will apply extension renaming on, all nested folders will be processed.")
+	print("m2: The new file extension: [String] The new file extension. If no filter is specified then all file extension are renamed.")
+	print("Optional parameters: ")
+	print("o1: The file extension filter: [String] Only apply the extension rename to files matching this filter.")
+	print("o2: Use Contains matching: [INT 1 or 0] When set to 1 the extension rename will be applied if the filter is contained in the rename candidate file. The default behavior is 0 and it relies on direct matching the filter criteria.")
 
-	for root, subdirs, files in os.walk(walk_dir):
+def goRename(baseDir,newExt,filter,matchingStrategy):
+	print('baseDir = ' + baseDir)
+	print('newExt = ' + newExt)
+	print('filter = ' + filter)
+	print('matchingStrategy = ' + str(matchingStrategy))
+	print('baseDir (absolute) = ' + os.path.abspath(baseDir))
+
+	for root, subdirs, files in os.walk(baseDir):
 
 		for filename in files:
 			file_path = os.path.join(root, filename)
-			base=os.path.basename(file_path)
-			thefile=os.path.splitext(base)[0]
+			base =  os.path.basename(file_path)
+			thefile = os.path.splitext(base)[0]
 			if renameExtFilter != '':			
-				if contains:
-					if os.path.splitext(base)[1].count(renameExtFilter)>-1:
-						os.rename(file_path, os.path.join(root, thefile+'.'+renameExt))
+				if matchingStrategy:
+					if os.path.splitext(base)[1].count(filter)>0:
+						os.rename(file_path, os.path.join(root, thefile+'.'+newExt))
 				else:
 
-					if os.path.splitext(base)[1]=='.'+renameExtFilter:
-						os.rename(file_path, os.path.join(root, thefile+'.'+renameExt))
+					if os.path.splitext(base)[1] == '.'+filter:
+						os.rename(file_path, os.path.join(root, thefile+'.'+newExt))
 			else:
-					os.rename(file_path, os.path.join(root, thefile+'.'+renameExt))
+					os.rename(file_path, os.path.join(root, thefile+'.'+newExt))
 
 
 if __name__ == "__main__":
 
-	if len(sys.argv) <2:
+	if len(sys.argv) <=2:
 		help()
 		exit()
 
-
-	walk_dir = sys.argv[1]
+	base_dir = sys.argv[1]
 	renameExt= sys.argv[2]
 	renameExtFilter= ''
 	contains=0
@@ -44,6 +51,6 @@ if __name__ == "__main__":
 		renameExtFilter=sys.argv[3]
 
 	if len(sys.argv) >4:
-		contains=sys.argv[4]
+		contains=int(sys.argv[4])
 
-	goRename(walk_dir,renameExt,renameExtFilter,contains)
+	goRename(base_dir,renameExt,renameExtFilter,contains)
